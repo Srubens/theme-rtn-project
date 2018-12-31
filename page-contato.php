0e3ca2bed40cd91687c9ba1059ac86d827faed03
Rubens Filipe <rubensfilipe6@gmail.com>
2018-12-31 17:38:56 -0300

    uploading contato and edit some files

---
+++ page-contato.php
@@ -0,0 +1,48 @@
+<?php  
+   get_header();
+?>
+
+<div class="container">
+   <div class="page_contato">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="row">
+               <div class="col-md-6">
+                  <header class="page_contato_header" >
+                     <h1>Entre em contato conosco tire suas duvidas!</h1>
+                  </header>
+                  <p>
+                        Mussum Ipsum, cacilds vidis litro abertis. 
+                        Aenean aliquam molestie leo, vitae iaculis nisl. 
+                        Quem num gosta di mim que vai caçá sua turmis! 
+                        Quem num gosta di mé, boa gentis num é.  
+                        Per aumento de cachacis, eu reclamis.
+                        </p>
+               </div>
+               <div class="col-md-6">
+                  <form class="form_page_contato" action="" method="">
+                     <div class="form-group">
+                        <input class="form-control" type="nome" placeholder="Meu Nome" required />
+                     </div>
+                     <div class="form-group">
+                        <input class="form-control" type="email" placeholder="Meu Email" required />
+                     </div>
+                     <div class="form-group">
+                        <input class="form-control" type="titulo" placeholder="Titulo da Mensagem" required />
+                     </div>
+                     <textarea class="form-control" name="" id="" cols="30" rows="10" required>
+                     </textarea>
+                     
+                     <input type="button" class="btn_contact btn btn-success" value="Enviar">
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+
+<?php  
+   get_footer();
+?>
--- page-produtos.php
+++ page-produtos.php
@@ -10,26 +10,45 @@
                 <form method="" action="" class="col-md-12">
                     <div class="form-group">
                         <div class="form_produtos">      
-                            <div class="col-md-8">
-                                <input class="form-control " type="text" placeholder="Pesquisar produto ex:argamassa">
-                            </div>
-                            <div class="col-md-4">
-                                <input class="btn btn-primary" type="button" value="aqui">
-                            </div>
+                            <input class="form-control " type="text" placeholder="Pesquisar produto ex:argamassa">
+                            <input class="btn btn-primary" type="button" value="Pesquisar">
                         </div>
                     </div>
                 </form>
                 
-                <div class="main_produtos_details">
-                    <h1>Nome do produto</h1>
-                    <img src="<?php bloginfo('template_url'); ?>/images/rejunto.jpg" alt="produto rejunto">
-                    <p>Descrição Breve do Produto, que chame a atenção do cliente!</p>
-                    <a class="btn btn-success" href="#">Veja Mais</a>
+                <?php
+                 
+                   $a = 0;
+                   for($a = 1; $a < 9; $a++){
+
+                ?>
+                
+                <div class="col-md-3">
+                    <div class="main_produtos_details">
+                        <h1>Nome do produto</h1>
+                        <a href="#">
+                            <img src="<?php bloginfo('template_url'); ?>/images/rejunto.jpg" alt="produto rejunto">
+                        </a>
+                        <p>Descrição Breve do Produto, que chame a atenção do cliente!</p>
+                        <a class="btn btn-success" href="#">Veja Mais</a>
+                    </div>
                 </div>
+
+                <?php
+                   }
+                ?>
 
             </div>
         </div>
     </div>
+
+    <div class="produtos_pre_next_page">
+        <div class="page_bottons">
+            <a href="#" class="btn_prev btn btn-outline-secondary">Pagina Anterior</a>
+            <a href="#" class="btn_next btn btn-outline-secondary">Proxima Pagina</a>
+        </div>
+    </div>
+
 </div>
 
 
